385db38dd883d1c2817fa395df1797a1c5ab0a14
chang <changxr@hzzh.com>
2017-04-12 16:54:07 +0800

    GitHub首次上传,添加左对齐和右对齐等布局方式；添加说明文件

---
+++ ImageTextButtonDemo/ImageTextButtonDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageTextButtonDemo
+//
+//  Created by Jonren on 15/12/30.
+//  Copyright © 2015年 Jonren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
